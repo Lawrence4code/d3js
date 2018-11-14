3a66bc7dfcc33e3cbb4690d77100d2529667101c
Lawrence <Lawrence4code@gmail.com>
2018-11-14 19:08:58 +0530

    Updates

---
+++ precache-manifest.9dd290a5bfc2cf4fc33ac59914ade412.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "119730bab11eba230dd8248c6b48a214",
+    "url": "/d3js/static/media/hero.119730ba.jpg"
+  },
+  {
+    "revision": "6a1332cd94fa0977fa63",
+    "url": "/d3js/static/js/runtime~main.6a1332cd.js"
+  },
+  {
+    "revision": "db434b9a700702b530b8",
+    "url": "/d3js/static/js/main.db434b9a.chunk.js"
+  },
+  {
+    "revision": "33bfe5d51d214b218140",
+    "url": "/d3js/static/js/1.33bfe5d5.chunk.js"
+  },
+  {
+    "revision": "db434b9a700702b530b8",
+    "url": "/d3js/static/css/main.dac1ef79.chunk.css"
+  },
+  {
+    "revision": "33bfe5d51d214b218140",
+    "url": "/d3js/static/css/1.b9aa0c5a.chunk.css"
+  },
+  {
+    "revision": "80e7055d1bd2693f65b8c3a307a7a365",
+    "url": "/d3js/index.html"
+  }
+];
