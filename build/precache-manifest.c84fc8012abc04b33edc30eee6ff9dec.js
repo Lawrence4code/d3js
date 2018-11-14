b6862dff444add54c9842cd5b1427987eee66f2d
Lawrence <Lawrence4code@gmail.com>
2018-11-14 17:48:13 +0530

    commit with build files

---
+++ build/precache-manifest.c84fc8012abc04b33edc30eee6ff9dec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "119730bab11eba230dd8248c6b48a214",
+    "url": "/static/media/hero.119730ba.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "db434b9a700702b530b8",
+    "url": "/static/js/main.db434b9a.chunk.js"
+  },
+  {
+    "revision": "33bfe5d51d214b218140",
+    "url": "/static/js/1.33bfe5d5.chunk.js"
+  },
+  {
+    "revision": "db434b9a700702b530b8",
+    "url": "/static/css/main.dac1ef79.chunk.css"
+  },
+  {
+    "revision": "33bfe5d51d214b218140",
+    "url": "/static/css/1.b9aa0c5a.chunk.css"
+  },
+  {
+    "revision": "826e82e10dd3c6df54970e3631ec003a",
+    "url": "/index.html"
+  }
+];
